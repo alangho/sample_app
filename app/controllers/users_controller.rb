5e5d554fc8e2442312f42c0a0ff1b94584460b7a
Alan <alangyho@gmail.com>
2015-03-02 21:42:17 -1000

    Password exercise implementations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,13 @@ class UsersController < ApplicationController
     redirect_to users_url
   end  
 
+  def index
+    @users = User.where(activated: true).paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
-    # debugger
+    redirect_to root_url and return unless @user.activated
   end
 
   def create
